408c861f1faa7451b893a32bc526a78ff8276d44
Yunai <daniel.colle0935@gmail.com>
2025-02-11 14:49:33 +0800

    'Hello'

--- Project A/First.py
+++ Project A/First.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+import math
+
+class piCalc:
+    def __init__(self,dec):
+        self.dec = dec
+
+
+    def calculate(self):
+        print("The value of dec is: ", self.dec)
+
+g1 = piCalc(2)
+g1.calculate()
