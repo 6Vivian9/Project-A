d70295be7d401b8efe82a80b4d822e6a2ab6ec90
Yunai <daniel.colle0935@gmail.com>
2025-02-14 18:13:18 +0800

    created function on getting the largest array and reverse array.

--- Project A/A.7/mean.py
+++ Project A/A.7/mean.py
@@ -1,12 +1,25 @@
 class mean:
     def __init__(self):
-        pass
+        self.a_list = [4,2,3,5,6,1]
+    def find_largest(self):
+        for i in range(0, len(self.a_list)):
+            if self.a_list[i] > self.a_list[i-1]:
+                self.temp = self.a_list[i]
+        print(self.temp)
 
-
+    def reverse_list(self):
+        a = -1
+        self.temp = [0] * len(self.a_list)
+        for i in range(0, len(self.a_list)):
+            self.temp[i] = self.a_list[a]
+            a = a-1
+        print(self.temp)
 
 
     def main(self):
-        print("Working")
+        mean.reverse_list()
+        
+
 
 if __name__ == "__main__":
     mean = mean()
